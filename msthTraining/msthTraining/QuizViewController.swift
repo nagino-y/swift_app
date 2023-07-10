8bb86e74c40c5d6a5cd9c36829430b9c0131e774
Yukichi <naginoyukichi@gmail.com>
2023-07-10 19:02:56 +0900

    mathtraining

---
+++ msthTraining/msthTraining/QuizViewController.swift
@@ -0,0 +1,67 @@
+//
+//  QuizViewController.swift
+//  msthTraining
+//
+//  Created by 奈木野諭吉 on 2023/07/10.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    
+    let total = 10 //問題数
+    var correct = 0 //正解数
+    var questionIndex = 0 //問題の番号
+    var answerIndex = 0 //正解の番号
+    
+    @IBOutlet var leftNumberLabel: UILabel!
+    @IBOutlet var centerNumberLabel: UILabel!
+    @IBOutlet var rightNumberLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setQuestions()
+    }
+    
+    override func didReceiveMemoryWarning(){
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    @IBAction func tapped(sender: UIButton){
+        if sender.tag -1 == answerIndex{
+            correct += 1
+        }
+        questionIndex += 1
+        if questionIndex >= total{
+            performSegue(withIndentifier: "QuizToResult", sender: nil)
+        }else {
+            setQuestions()
+        }
+    }
+    
+    func setQuestions() {
+        let leftNum = Int(arc4random_uniform(10))
+        var centerNum = Int(arc4random_uniform(10))
+        
+        answerIndex = Int(arc4random_uniform(4))
+        switch answerIndex {
+        case 0:
+            rightNumberLabel.text = "\(leftNum + centerNum)"
+        case 1:
+            rightNumberLabel.text = "\(leftNum - centerNum)"
+        case 2:
+            rightNumberLabel.text = "\(leftNum * centerNum)"
+        default:
+            if centerNum == 0 {
+                centerNum = 1
+            }
+            rightNumberLabel.text = "\(leftNum / centerNum)"
+        }
+        leftNumberLabel.text = "\(leftNum)"
+        centerNumberLabel.text = "\(centerNum)"
+    }
+
+}
---
+++ msthTraining/msthTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  msthTraining
+//
+//  Created by 奈木野諭吉 on 2023/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
