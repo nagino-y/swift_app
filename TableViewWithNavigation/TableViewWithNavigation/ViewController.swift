24beb19106c5cb9ca0270fa2c6d738fe50926f14
Yukichi <naginoyukichi@gmail.com>
2023-07-31 23:59:32 +0900

    animals

--- TableViewWithNavigation/TableViewWithNavigation/ViewController.swift
+++ TableViewWithNavigation/TableViewWithNavigation/ViewController.swift
@@ -7,11 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let animals = ["Dog 🐶", "Cat 😺", "Panda 🐼", "Lion 🦁", "Snake 🐍"]
+    
+    @IBOulet var animalTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        animalTableView.detaSource = self
+        animalTableView.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        retrun animals.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        
+        let label = cell.viewWithTag(1) as? UILabel
+        label?.text = animals[indexPath.row]
+        
+        return cell
     }
 
 
