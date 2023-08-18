b2ab2ac4af8a1d1c5ddb929c3af92b46307edfd8
Yukichi <naginoyukichi@gmail.com>
2023-08-18 22:45:55 +0900

    swift app

---
+++ TwitterSwiftUIYoutube/TwitterSwiftUIYoutube/TwitterSwiftUIYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUIYoutubeApp.swift
+//  TwitterSwiftUIYoutube
+//
+//  Created by 奈木野諭吉 on 2023/08/18.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUIYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
